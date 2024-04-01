1dfea18e890e801f7eed016854250325d19a270b
Amogh <133480707+Amoghcjc@users.noreply.github.com>
2024-04-01 14:27:57 +0530

    Update Hello.java

--- 77777Amogh/src/CCCC/Hello.java
+++ 77777Amogh/src/CCCC/Hello.java
@@ -2,7 +2,7 @@ package CCCC;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("raje0707");
+	System.out.println("raje007");
 	
 	System.out.println("hello raje");
 	System.out.println("hello raje");
